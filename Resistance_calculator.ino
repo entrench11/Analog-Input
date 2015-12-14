035bd7d7300c410fc1021bbaf8e437d4c58f9923
entrench11 <info.entrench@gmail.com>
2015-12-14 15:22:52 +0530

    Create Resistance_calculator.ino

---
+++ Resistance_calculator.ino
@@ -0,0 +1,18 @@
+int analog=A1;
+int analogValue=0;
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  analogValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float resistor = analogValue * (5.0 / 1023.0);
+  // print out the value you read:
+  Serial.print("Resistance: ");
+  Serial.println(resistor);
+  delay(100);
+}
